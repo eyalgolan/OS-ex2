8da6ea02a22b0037efb1530fe246e100f4d4cc96
eyalgolan <eyalgolan96@gmail.com>
2020-04-27 18:32:34 +0300

    create basic program structure

--- ex2.c
+++ ex2.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+char *get_command(void) {
+
+}
+
+char **get_args(char *command) {
+
+}
 void command_loop(void) {
-    printf("> ");
+    char *command;
+    char **command_args;
+    int status;
+
+    do {
+        printf("> ");
+        command = get_command();
+        command_args = get_args(command);
+
+        free(command);
+        free(command_args);
+    } while(status);
 }
 
 int main() {
